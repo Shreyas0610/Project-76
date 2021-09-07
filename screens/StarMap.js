21e4ce9b087ab51a804cc33d93ad4ab3dec423b5
Shreyas0610 <67569500+Shreyas0610@users.noreply.github.com>
2021-09-06 21:11:40 -0500

    Add files via upload

---
+++ screens/StarMap.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class StarMapScreen extends Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                <Text>Star Map Screen!</Text>
+            </View>
+        )
+    }
+}
